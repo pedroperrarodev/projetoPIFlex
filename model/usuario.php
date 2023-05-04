d2f5ae09e73ce2ed8ec54d70307a129a017fec97
Pedrin7 <pedropaim987@gmail.com>
2023-05-04 09:24:45 -0300

    continuando o projeto pi, feita algumas alterações, anotações e modificações

--- model/usuario.php
+++ model/usuario.php
@@ -1,4 +1,35 @@
 <?php
-    require_once ("");
+    require_once ("../infra/database.php");
+
+    class usuario{
+       
+        private $nome_completo;
+        private $cpf;
+        private $cep;
+        private $num_telefone;
+        private $email;
+        private $senha;
+
+        public function __set($atributo, $valor)
+        {
+           if(property_exists($this,$atributo)){
+            $this->$atributo = $valor;
+           } 
+        }
+
+        public function __get($atributo)
+        {
+           if(property_exists($this,$atributo)){
+            return $this->$atributo;
+           } 
+        }
+
+        public function __construct()
+        {
+            
+        }
+
+        
+    }
 
 ?>
