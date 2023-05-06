5629196670120b6234f9cebf9bfe109fc2db5242
Pedrin7 <pedropaim987@gmail.com>
2023-05-06 19:26:49 -0300

    tentativa acao cadastro, sem sucesso, imprime a página php com o código inteiro

--- controller/usuario_controller.php
+++ controller/usuario_controller.php
@@ -1,12 +1,61 @@
 <?php
 
+    require_once ("../model/usuario.php");
+
     class usuario_controller{
 
         public function execute($post, $get){
             $acao = $get['acao'];
-            //continuar
+            if($acao == "cadastrar"){
+                $usuario = new usuario();
+
+                if ($acao == "cadastrar"){
+                    $usuario = new Usuario();
+    
+                    $nome_completo = $post["nome_completo"];
+                    $usuario->__set("nome_completo", $nome_completo);
+    
+                    $cpf = $post["cpf"];
+                    $usuario->__set("cpf", $cpf);
+
+                    $rua = $post["rua"];
+                    $usuario->__set("rua", $rua);
+
+                    $bairro = $post["bairro"];
+                    $usuario->__set("bairro", $bairro);
+    
+                    $cidade = $post["cidade"];
+                    $usuario->__set("cidade", $cidade);
+
+                    $numero = $post["numero"];
+                    $usuario->__set("numero", $numero);
+
+                    $num_telefone = $post["num_telefone"];
+                    $usuario->__set("num_telefone", $num_telefone);
+
+                    $email = $post["email"];
+                    $usuario->__set("email", $email);
+
+    
+                    $senha = $post["senha"];
+                    $confirmar_senha = $post["conf_senha"];
+                    
+                    if ($senha == $confirmar_senha){
+                        $senha_hash = hash("sha3-256", $senha);
+                        $usuario->__set("senha", $senha_hash);
+                       
+                        if($usuario->cadastrar() == true){
+                            echo "Usuario Cadastrado com Sucesso!";
+                        }
+                    }
+                    else{
+                        //enviar msg de erro
+                    }
+            }
+            
         }
     }
 
+}
 
 ?>
--- model/usuario.php
+++ model/usuario.php
@@ -27,9 +27,30 @@
            } 
         }
 
-        public function __construct()
-        {
-            
+        public function cadastrar(){
+            $db = new database();
+            $con = $db->connect();
+
+            $sql = "INSERT INTO adm_e_usuario(nome_completo, cpf, rua, bairro, cidade, numero, num_telefone, email, senha, perfil) 
+            VALUES(:nome_completo, :cpf, :rua, :bairro, :cidade, :numero, :num_telefone, :email, :senha,:perfil = 2)";
+
+            $st = $con->prepare($sql);
+            $st->bindParam(':nome_completo', $this->nome_completo);
+            $st->bindParam(':cpf', $this->cpf);
+            $st->bindParam(':rua', $this->rua);
+            $st->bindParam(':bairro', $this->bairro);
+            $st->bindParam(':cidade', $this->cidade);
+            $st->bindParam(':numero', $this->numero);
+            $st->bindParam(':num_telefone', $this->num_telefone);
+            $st->bindParam(':email', $this->email);
+            $st->bindParam(':senha', $this->senha);
+            $status = $st->execute();
+
+            $idUsuario = $con->lastInsertId();
+
+             if ($status == true){
+                 return true;
+            }
         }
 
         
