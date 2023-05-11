319b38f1477f62899d1909bd311e1c17ff4991d3
pedroperrarodev <pedroperrarodev@gmail.com>
2023-05-11 19:23:58 -0300

    a

--- view/usuario/tela_profile.php
+++ view/usuario/tela_profile.php
@@ -5,15 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <HEAD:view/usuario/index.html></HEAD:view>
-    <!-- CSS PARA O HTML -->
     <link rel="stylesheet" type="text/css" href="../../css/tela_profile.css">
     <link rel="stylesheet" type="text/css" href="../../css/corpo.css">
 
-    <!-- CSS PARA O PHP CONTROLLER -->
-    <link rel="stylesheet" type="text/css" href="../css/tela_profile.css">
-    <link rel="stylesheet" type="text/css" href="../css/corpo.css">
-
-    <!-- TELA DE USUARIO -->
+    
     <title>Cadastro de Vacinas</title>
 </head>
 <body>  
@@ -36,10 +31,10 @@
         <div class="menu">
             <div class="sidebar">
                 <div>
-                    <a href="tela_profile_usuario.php"><button id="activemenu"><strong>Perfil</strong></button></a>
+                    <a href="tela_profile.html"><button id="activemenu"><strong>Perfil</strong></button></a>
                 </div>
                 <div>
-                    <a href="cadastro_vacina.php"><button><strong>Cadastro de Vacinas </strong></button></a>
+                    <a href="cadastro_vacina.html"><button><strong>Cadastro de Vacinas </strong></button></a>
                 </div>
                 <div>
                     <a><button><strong>Menu 3</strong></button></a>
@@ -73,20 +68,20 @@
                     <th>Nome da vacina</th>
                     <th>Local</th>
                     <th>Fabricante</th>
-                    <th>Função Vacina</th>
+                    <th>Lote</th>
                 </tr>
                 
                 <?php
-                // for ($i=0; $i<sizeof($dados);$i++){
-                //     echo "<tr>";	
-                //     /* echo "<td><a href=\"../../../controller/usuario_controller.php?acao=editar&id=".$dados[$i]["id"]."\">".$dados[$i]["id"]."</a></td>"; */
-                //     echo "<td>".$dados[$i]["nomevacina"]."</td>";
-                //     echo "<td>".$dados[$i]["local"]."</td>";
-                //     echo "<td>".$dados[$i]["fabricante"]."</td>";
-                //     echo "<td>".$dados[$i]["funcao_vacina"]."</td>";
-                //     /* echo "<td align='center'><a href=\"../../../controller/usuario_controller.php?acao=deletar&id=".$dados[$i]["id"]."\">x</a></td>"; */
-                //     echo "</tr>";
-                // }
+                for ($i=0; $i<sizeof($dados);$i++){
+                    echo "<tr>";	
+                    /* echo "<td><a href=\"../../../controller/usuario_controller.php?acao=editar&id=".$dados[$i]["id"]."\">".$dados[$i]["id"]."</a></td>"; */
+                    echo "<td>".$dados[$i]["nomevacina"]."</td>";
+                    echo "<td>".$dados[$i]["local"]."</td>";
+                    echo "<td>".$dados[$i]["fabricante"]."</td>";
+                    echo "<td>".$dados[$i]["lote"]."</td>";
+                    /* echo "<td align='center'><a href=\"../../../controller/usuario_controller.php?acao=deletar&id=".$dados[$i]["id"]."\">x</a></td>"; */
+                    echo "</tr>";
+                }
                 ?>
                 
 
@@ -96,7 +91,7 @@
        </div>
        <div class="containerprofile2">
         <div>
-            <a><img src="../img/profileicon.png"></a><br>
+            <a><img src="../../img/profileicon.png"></a><br>
             <h3>Bem vindo</h3>
             <p>Nome de profile</p>
             <p>Informações de profile</p>
@@ -104,7 +99,7 @@
         </div>
 
         <!-- <a href="tela_configusuario.html"><img id="iconeconfig" src="../../img/iconconfig.png"></a> -->
-        <a href="tela_configusuario.html"><button id="buttonconfig"><img id="iconeconfig" src="../img/iconconfig.png"></button></a>
+        <a href="tela_configusuario.html"><button id="buttonconfig"><img id="iconeconfig" src="../../img/iconconfig.png"></button></a>
 
 
 
